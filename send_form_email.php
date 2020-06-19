bfa2c09f4fc7ba4cd86d24bfa0a03265772cd624
Vivek Domadia <45758448+VivekDomadia@users.noreply.github.com>
2020-06-19 09:27:07 +0530

    Add files via upload

--- send_form_email.php
+++ send_form_email.php
@@ -13,9 +13,7 @@
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
 
@@ -46,9 +44,7 @@
         }
 
     } else {
-        // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
     }
 
 ?>
