39f562be9bd4d0a34a89a368fd02a06b635f7980
VivekDomadia <domadia.vivek.2@ldce.ac.in>
2020-07-16 16:26:40 +0530

    some changes

--- send_form_email.php
+++ send_form_email.php
@@ -1,8 +1,7 @@
 <?php
 
-    // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
+
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
         $name = $fname.' '.$lname;
@@ -11,40 +10,33 @@
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
-        // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
+            echo "Please fill all required field.";
             exit;
         }
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "domadia.vivek11@gmail.com";
+        $recipient = "me@vivekdomadia.engineer";
 
-        // Set the email subject.
         $subject = "New contact from $name";
 
-        // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
         $email_headers = "From: $name <$email>";
 
-        // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
-            // Set a 500 (internal server error) response code.
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         http_response_code(403);
+        echo "Oops! Something went wrong.";
     }
 
 ?>
